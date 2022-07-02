e90b7bac07ef10386f1061672106b02852179c59
Deemou <4106243@naver.com>
2022-07-02 23:09:08 +0900

    6.16 Trello Clone Before Code Challenge

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,31 +1,19 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
 
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-export interface IToDo {
-  text: string;
+export interface ITodo {
   id: number;
-  category: Categories;
+  text: string;
 }
 
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
-});
+export interface IToDoState {
+  [key: string]: ITodo[];
+}
 
-export const toDoState = atom<IToDo[]>({
+export const toDoState = atom<IToDoState>({
   key: "toDo",
-  default: [],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
-  get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+  default: {
+    "To Do": [],
+    Doing: [],
+    Done: [],
   },
 });
