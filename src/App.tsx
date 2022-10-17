7f9d4d0f85d582cb17d34de94febdf80cade5185
Deemou <4106243@naver.com>
2022-10-17 13:54:33 +0900

    #8.2 Header part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./Components/Header";
 import Home from "./Routes/Home";
 import Search from "./Routes/Search";
 import Tv from "./Routes/Tv";
